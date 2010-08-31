ef3e37075af3ea78db4b604414fc8c5622d92533
Owen Shepherd <owen.shepherd@e43.eu>
2010-08-31 03:24:15 +0100

    Add Egg installation script contributed by Quok. Now a bonafide Python package =)

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+ 
+version = '0.1'
+ 
+setup(
+    name = "TCWiki",
+    version = version,
+    description = "Django wiki engine with Creole Syntax",
+    long_description = "Django wiki engine using Creole syntax, designed to integrate with with other applications",
+    keywords = 'web wiki django creole',
+    license = 'ISC',
+    author = 'Element43 & EForge Project',
+    author_email = 'support@e43.eu',
+    url = '',
+    zip_safe = True,
+    include_package_data = True,
+    install_requires = [
+	"django>=1.2.1",
+        "creoleparser>=0.7.2"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: ISC License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
